d35d2b57431291ba3b423e1655c29ebc81ac70a0
RyoJerryYu <tzl19961216@gmail.com>
2021-12-03 23:35:41 +0800

    refactor(members): rename for more simplizer

--- members.py
+++ members.py
@@ -8,20 +8,20 @@ memberRe = re.compile(r'\W*(.+);\W*')
 
 
 class Member:
-  '''The member format of a Cpp class or Golang struct.
+  '''The member pattern of a Cpp class or Golang struct.
 
   Example:
     '    int a;' -> '    a int'
     '''
-  tvpair_pattern: Pattern
+  tvpair_p: Pattern
 
-  def __init__(self, tvpair_pattern: Pattern):
-    self.tvpair_pattern = tvpair_pattern
+  def __init__(self, tvpair_p: Pattern):
+    self.tvpair_p = tvpair_p
 
   def transform(self, source: str):
-    tvpair = memberRe.findall(source)[0]
+    tvpair_p = memberRe.findall(source)[0]
 
-    res_tvpair = self.tvpair_pattern.transform(tvpair)
+    res_tvpair = self.tvpair_p.transform(tvpair_p)
 
     return f'    {res_tvpair}'
 
@@ -30,37 +30,37 @@ tvpairRe = re.compile(r'\W*([a-zA-Z0-9_\*]+)\W+(\w+)\W*')
 
 
 class TVPair:
-  '''The type value pair format.
+  '''The type value pair pattern.
 
   Example:
     'int a' -> 'a int'
   '''
-  type_pattern: Pattern
-  value_pattern: Pattern
+  type_name_p: Pattern
+  var_p: Pattern
 
-  def __init__(self, type_pattern: Pattern, value_pattern: Pattern):
-    self.type_pattern = type_pattern
-    self.value_pattern = value_pattern
+  def __init__(self, type_name_p: Pattern, var_p: Pattern):
+    self.type_name_p = type_name_p
+    self.var_p = var_p
 
   def transform(self, source: str):
-    type, value = tvpairRe.findall(source)[0]  # pylint: disable=redefined-builtin
+    type, variable = tvpairRe.findall(source)[0]  # pylint: disable=redefined-builtin
 
-    res_type = self.type_pattern.transform(type)
-    res_value = self.value_pattern.transform(value)
+    res_type = self.type_name_p.transform(type)
+    res_variable = self.var_p.transform(variable)
 
-    return f'{res_value} {res_type}'
+    return f'{res_variable} {res_type}'
 
 
-class TypeDclr:
-  '''The type format.
+class TypeName:
+  '''The type pattern.
 
   Example:
     'card_info*' -> '*cardInfo'
   '''
-  word_pattern: Pattern
+  word_p: Pattern
 
-  def __init__(self, word_pattern: Pattern):
-    self.word_pattern = word_pattern
+  def __init__(self, word_p: Pattern):
+    self.word_p = word_p
 
   def transform(self, source: str):
     star = '*' if source[-1] == '*' else ''
@@ -68,34 +68,34 @@ class TypeDclr:
 
     res_words = [words[0]]
     for word in words[1:]:
-      res_words.append(self.word_pattern.transform(word))
+      res_words.append(self.word_p.transform(word))
 
     return star + ''.join(res_words)
 
 
 class Variable:
-  '''The variable format.
+  '''The variable pattern.
 
   Example:
     'card_info' -> 'cardInfo'
   '''
-  word_pattern: Pattern
+  word_p: Pattern
 
-  def __init__(self, word_pattern: Pattern):
-    self.word_pattern = word_pattern
+  def __init__(self, word_p: Pattern):
+    self.word_p = word_p
 
   def transform(self, source: str):
     words = source.split('_')
 
     res_words = [words[0]]
     for word in words[1:]:
-      res_words.append(self.word_pattern.transform(word))
+      res_words.append(self.word_p.transform(word))
 
     return ''.join(res_words)
 
 
-class Word:
-  '''The word format. Just capital the first character.
+class Capitalizer:
+  '''The word pattern. Just capital the first character.
 
   Example:
     'card' -> 'Card'
@@ -107,7 +107,7 @@ class Word:
 
 if __name__ == '__main__':
   m = Member(TVPair(
-      TypeDclr(Word()),
-      Variable(Word()),
+      TypeName(Capitalizer()),
+      Variable(Capitalizer()),
   ))
   print(m.transform('    card_type* a_card;'))
