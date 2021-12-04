71452d65167315eb2f82ccfc79e6822299ac30a6
RyoJerryYu <tzl19961216@gmail.com>
2021-12-04 13:50:24 +0800

    feat(methods): add default values for methods

--- methods.py
+++ methods.py
@@ -4,34 +4,9 @@ import re
 from typing import List
 
 from protocol import Pattern
+from common import Capitalizer, TVPair, TypeName
 
 methodRe = re.compile(r'\W*([a-zA-Z0-9_\*]+)\W+(\w+)\((.*)\);\W*')
-
-
-class Method:
-  '''The class method pattern.
-
-  Example:
-    '    int foo(int a, int b);'
-  ->'    foo(a int, b int) int'
-    '''
-  type_name: Pattern
-  fname: Pattern
-  tvlist: Pattern
-
-  def __init__(self, type_name: Pattern, fname: Pattern, tvlist: Pattern):
-    self.type_name = type_name
-    self.fname = fname
-    self.tvlist = tvlist
-
-  def transform(self, source: str) -> str:
-    type, func, tvlist = methodRe.findall(source)[0]  # pylint: disable=redefined-builtin
-
-    res_type = self.type_name.transform(type)
-    res_func = self.fname.transform(func)
-    res_tvlist = self.tvlist.transform(tvlist)
-
-    return f'    {res_func}({res_tvlist}) {res_type}'
 
 
 class FuncName:
@@ -42,7 +17,7 @@ class FuncName:
   '''
   word_p: Pattern
 
-  def __init__(self, word_p: Pattern):
+  def __init__(self, word_p: Pattern = Capitalizer()):
     self.word_p = word_p
 
   def transform(self, source: str) -> str:
@@ -63,7 +38,7 @@ class TVList:
   '''
   tvpair_p: Pattern
 
-  def __init__(self, tvpair_p: Pattern):
+  def __init__(self, tvpair_p: Pattern = TVPair()):
     self.tvpair_p = tvpair_p
 
   def transform(self, source: str) -> str:
@@ -74,3 +49,32 @@ class TVList:
       res_tvpairs.append(self.tvpair_p.transform(tvpair))
 
     return ', '.join(res_tvpairs)
+
+
+class Method:
+  '''The class method pattern.
+
+  Example:
+    '    int foo(int a, int b);'
+  ->'    foo(a int, b int) int'
+    '''
+  type_name: Pattern
+  fname: Pattern
+  tvlist: Pattern
+
+  def __init__(self,
+               type_name: Pattern = TypeName(),
+               fname: Pattern = FuncName(),
+               tvlist: Pattern = TVList()):
+    self.type_name = type_name
+    self.fname = fname
+    self.tvlist = tvlist
+
+  def transform(self, source: str) -> str:
+    type, func, tvlist = methodRe.findall(source)[0]  # pylint: disable=redefined-builtin
+
+    res_type = self.type_name.transform(type)
+    res_func = self.fname.transform(func)
+    res_tvlist = self.tvlist.transform(tvlist)
+
+    return f'    {res_func}({res_tvlist}) {res_type}'
--- methods_test.py
+++ methods_test.py
@@ -4,17 +4,33 @@ from common import Capitalizer
 from protocol import DirectReturn
 
 
-def test_method():
-  method_p = Method(DirectReturn(), DirectReturn(), DirectReturn())
-  assert method_p.transform(
-      '    int foo(int a, int b);') == '    foo(int a, int b) int'
-
-
 def test_funcname():
   funcname_p = FuncName(Capitalizer())
+  assert funcname_p.transform('foo_bar_baz') == 'FooBarBaz'
+
+
+def test_funcname_default():
+  funcname_p = FuncName()
   assert funcname_p.transform('foo_bar_baz') == 'FooBarBaz'
 
 
 def test_tvlist():
   tvlist_p = TVList(DirectReturn())
   assert tvlist_p.transform('int a, int b') == 'int a, int b'
+
+
+def test_tvlist_default():
+  tvlist_p = TVList()
+  assert tvlist_p.transform('int a, int b') == 'a int, b int'
+
+
+def test_method():
+  method_p = Method(DirectReturn(), DirectReturn(), DirectReturn())
+  assert method_p.transform(
+      '    int foo(int a, int b);') == '    foo(int a, int b) int'
+
+
+def test_method_default():
+  method_p = Method()
+  assert method_p.transform(
+      '    int foo(int a, int b);') == '    Foo(a int, b int) int'
